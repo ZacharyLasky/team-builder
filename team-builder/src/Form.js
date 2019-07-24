e14aecd82fc5f3dbcd5b92e279fa685c2968a365
Zach Lasky <zacharylasky123@gmail.com>
2019-07-24 19:01:11 -0400

    added form with mock values

---
+++ team-builder/src/Form.js
@@ -0,0 +1,91 @@
+import React, { useState } from 'react';
+
+export default function Form() {
+  const [team, setTeam] = useState([
+    
+    {
+    name: "zach",
+    email: "zach@gmail.com",
+    role: "dev"
+    },
+
+    {
+    name: "d",
+    email: "z@z.com",
+    role: "dev"
+    }
+  ])
+  
+  const [form, setForm] = useState({
+    name: "",
+    email: "",
+    role: ""
+  })
+
+  function handleChange(event) {
+    const updatedForm = { ...form, [event.target.name]: event.target.value };
+    console.log(
+      "handleChange",
+      event.target.name,
+      event.target.value,
+      updatedForm
+    );
+     setForm(updatedForm);
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    console.log(form.name);
+    console.log(form.email);
+    console.log(form.role);
+  }
+  
+  return (
+    <div className="form-container">
+      <form onSubmit={handleSubmit}>
+        <label>
+          name:
+          <input
+            type="text" onChange={handleChange}
+            className="form-info"
+            name="name"
+            placeholder="enter name"
+            value={form.name}
+          />
+        </label>
+        <label>
+          email:
+          <input
+            type="text" onChange={handleChange}
+            className="form-info"
+            name="email"
+            placeholder="enter email"
+            value={form.email}
+          />
+        </label>
+        <label>
+          role:
+          <input
+            type="text" onChange={handleChange}
+            className="form-info"
+            name="role"
+            placeholder="enter role"
+            value={form.role}
+          />
+        </label>
+        <button type="submit">Submit!</button>
+      </form>
+
+      {team.map(vals => (
+        <div key={Math.random() * 100000000}>
+        {vals.name}
+        {vals.email}
+        {vals.role}
+      </div>
+      )
+        
+      )}
+      
+    </div>
+  )
+}
